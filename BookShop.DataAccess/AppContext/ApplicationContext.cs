72c9f5d47499da628f9d23deb5b3eaf9b7564ded
A.Lytvynenko <A.Lytvynenko.Work@gmail.com>
2019-08-27 17:58:45 +0300

    Evening Commit

---
+++ BookShop.DataAccess/AppContext/ApplicationContext.cs
@@ -0,0 +1,9 @@
+﻿using BookShop.DataAccess.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace BookShop.DataAccess.AppContext
+{
+    public class ApplicationContext:IdentityDbContext<ApplicationUser>
+    {
+    }
+}
---
+++ BookShop.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.BusinessLogic.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<UserModel> _userManager;
+        private readonly SignInManager<UserModel> _singInManager;
+
+        public AccountController(UserManager<UserModel> userManager, SignInManager<UserModel> signInManager)
+        {
+            this._userManager = userManager;
+            this._singInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
